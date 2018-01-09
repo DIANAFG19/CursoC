e5b8a9d973db0ae52619a18a6f700f709589d07e
Diana Flores García <diana_1310fg@hotmail.com>
2018-01-09 13:57:48 -0600

    Menú de Inicio.

---
+++ main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: YOO
+ * Created on 9 de enero de 2018, 01:41 PM
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    int opcion = 0;
+    printf("\t\t***** BIENVENIDO AL CURSO DE C *****");
+    printf("\n\t PROGRAMAS:\n\n");
+    printf("1. Suma de dos números.\n2. Datos de usuario.\n3. ¿Positivo o negativo?.\n4.¿Pares o impares?.\n5. Conversión.\n"
+           "6. Calificaciones.\n7. Calculadora.\n8. Uso del FOR.\n9. Fómula general.\n10. Tablas de multiplicar.\n11. Arreglos.\n"
+           "12. Conexión a Base de Datos (MySQL).\n13. Salir.\n\n\tEliga una opción [1-13]: ");
+    //scanf("%i",&opcion);
+    
+
+    return (EXIT_SUCCESS);
+}
+
