195b94d01bc4298e839a4823b58439ba10834127
Diana <diana1310fg@gmail.com>
2018-09-13 23:22:29 -0500

    Introducción a C, Tipo de Datos y Variables.
    Introducción a C, Tipo de Datos y Variables.

---
+++ DataTypes.c
@@ -0,0 +1,16 @@
+/*
+Creador: Ing. Flores García Diana Laura.
+Fecha de creación: 13/09/18 23:13 hrs.
+Programa 2: Data Types.
+*/
+#include <stdio.h>
+
+int main(){
+	
+	printf("int: %d \n", sizeof(int));
+	printf("float: %d \n", sizeof(float));
+	printf("double: %d \n", sizeof(double));
+	printf("char: %d \n", sizeof(char));
+	
+	return 0;
+}
---
+++ Variables.c
@@ -0,0 +1,22 @@
+/*
+Creador: Ing. Flores García Diana Laura.
+Fecha de creación: 13/09/18 23:17 hrs.
+Programa 3: Variables.
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	int a, b;
+	float salario = 56.23;
+	char letra = 'Z';
+	a = 8;
+	b = 34;
+	int c = a + b;
+	printf("%d \n", c);
+	printf("%f \n", salario);
+	printf("%c \n", letra);
+	
+	return 0;
+}
