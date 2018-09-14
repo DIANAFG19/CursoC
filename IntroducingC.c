7192f79caa807e97fc39886bd6f080e21f454bb4
Diana <diana1310fg@gmail.com>
2018-09-13 23:08:30 -0500

    Introducing C.
    Introducing C.

---
+++ IntroducingC.c
@@ -0,0 +1,14 @@
+/*
+Creador: Ing. Flores García Diana Laura.
+Fecha de creación: 13/09/18 22:59 hrs.
+Objetivo: Comenzar a programar en lenguaje de medio nivel C.
+*/
+
+//Programa 1: Hello World!
+
+#include <stdio.h> //Librería para entrada y salida de datos printf y scanf.
+
+int main(){
+	printf("Hello World!\n");
+	return 0;
+}
